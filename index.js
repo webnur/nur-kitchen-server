2fa38d2b6319dfb06c2c14cf9b1c21cb5e7db1af
webnur <mdnuruli291@gmai.com>
2022-11-09 10:47:13 +0600

    assignment-11 server start

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config
+const port = process.env.PORT || 5000;
+const app = express()
+
+// middleware
+app.use(cors())
+app.use(express.json())
