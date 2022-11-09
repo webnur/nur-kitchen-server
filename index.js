b78dd592af57fe4293dfb7d4a2477b1d0a30107d
webnur <mdnuruli291@gmai.com>
2022-11-09 12:19:20 +0600

    mongodb server connected alhamdulillah

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 const app = express()
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+//username: nuurkitchen
+//password: VHQY2hrLXVodJlWa
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6x8xxck.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri)
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('assignment 11 server is open')
+})
+
+app.listen(port, () => console.log(`app is open on port ${port}`))
