c7bb5e652478d32d6ddde8ef8f928dabefedcc21
webnur <mdnuruli291@gmai.com>
2022-11-09 21:40:24 +0600

    service search with id added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const app = express()
@@ -14,16 +14,28 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run () {
+async function run() {
 
- const servicesCollection = client.db('nurKitchen').collection('services');
+    try {
+        const servicesCollection = client.db('nurKitchen').collection('services');
 
- app.get('/services', async(req, res) => {
-    const query = {};
-    const cursor = servicesCollection.find(query);
-    const services = await cursor.toArray();
-    res.send(services)
- })
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service)
+        })
+    }
+    finally {
+
+    }
 
 }
 run().catch(error => console.log(error))
