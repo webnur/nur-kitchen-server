7b3509487e71213ddcd48493679cb73a98880b80
webnur <mdnuruli291@gmai.com>
2022-11-09 12:32:40 +0600

    services data saved on mongodb and host on server side

--- index.js
+++ index.js
@@ -9,19 +9,24 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-//username: nuurkitchen
-//password: VHQY2hrLXVodJlWa
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6x8xxck.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri)
+async function run () {
 
+ const servicesCollection = client.db('nurKitchen').collection('services');
 
+ app.get('/services', async(req, res) => {
+    const query = {};
+    const cursor = servicesCollection.find(query);
+    const services = await cursor.toArray();
+    res.send(services)
+ })
 
+}
+run().catch(error => console.log(error))
 
 
 app.get('/', (req, res) => {
