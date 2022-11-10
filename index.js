8961fb59a76f1d863b04cdd6ac17b0603d56693f
webnur <mdnuruli291@gmai.com>
2022-11-10 22:41:23 +0600

    server side done alhamdulillah

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6x8xxck.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
